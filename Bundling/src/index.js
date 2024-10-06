094465fafa48b9f9c1662f8fe478b9b67558b2ac
andrea <andrea@uptodown.com>
2024-10-06 12:55:04 +0200

    basic content

---
+++ Bundling/src/index.js
@@ -0,0 +1,10 @@
+import * as service from "./averageService";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.scss"
+import logoImg from "./content/logo.png";
+
+
+
+const img = document.createElement("img");
+img.src = logoImg;
+document.getElementById("imgContainer").appendChild(img);
