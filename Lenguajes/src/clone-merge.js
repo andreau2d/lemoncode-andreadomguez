9badac783d1261f8d6485718a05db39a365c4cb9
andrea <andrea@uptodown.com>
2024-10-06 13:14:26 +0200

    delete duplicates

--- Lenguajes/src/clone-merge.js
+++
@@ -1,9 +0,0 @@
-function clone(source) {
-    return {...source};
-  }
-
-function merge(source, target) { 
-  const sourceCloned = clone(source);
-  const targetCloned = clone(target);
-  return {... targetCloned, ...sourceCloned}; //se añade primero target para darle prioridad al array source
-}
--- Lenguajes/src/concat.js
+++
@@ -1,7 +0,0 @@
-const concat = (a, b) => {
-   return [...a, ...b]
-}
-
-const optionalConcat = ( ...arrays) => {
-  return arrays.reduce((result, array) => [...result, ...array], []);
-}
